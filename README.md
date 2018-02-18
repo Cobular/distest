c4fa6e55248f4173249715ccaf777a166709123a
DXsmiley <dxsmiley@hotmail.com>
2018-02-18 21:08:07 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dismock
+
+A small library used to write automated test for Discord bots.
+
+Currently in version `0.0.0`, since I'm still getting things set up in here.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='dismock',
+      version='0.0.0',
+      description='Automate the testing of discord bots',
+      url='http://github.com/DXsmiley/dismock',
+      author='DXsmiley',
+      license='MIT',
+      packages=['dismock'],
+      install_requires=['discord.py'],
+      zip_safe=False
+)
