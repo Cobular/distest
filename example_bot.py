a25cdd13cd48f38fbcff6cb79dd76de3555391cc
Jake Cover <Royalcows9@gmail.com>
2019-04-14 19:41:58 -0700

    Updated the two bots to be compliant with new discord.py

--- example_bot.py
+++ example_bot.py
@@ -1,21 +1,25 @@
-'''
+"""
     Run with:
         python example_bot.py TARGET_TOKEN
-'''
+"""
 
 import sys
+
 import discord
 
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
     print('Ready')
 
+
 @client.event
 async def on_message(message):
     if message.content == 'ping?':
         print('Replying')
-        await client.send_message(message.channel, 'pong!')
+        await message.channel.send('pong!')
+
 
 client.run(sys.argv[1])
--- example_tests.py
+++ example_tests.py
@@ -1,21 +1,29 @@
-'''
-	Run with:
-		python example_tests.py TARGET_NAME TESTER_TOKEN
-'''
+"""
+    Run with:
+        python example_tests.py TARGET_NAME TESTER_TOKEN
+"""
 
 import sys
 import dismock
 
 # The tests themselves
 
-testcollector = dismock.TestCollector()
+test_collector = dismock.TestCollector()
 
-@testcollector()
+
+@test_collector()
 async def test_ping(interface):
-	await interface.assert_reply_equals('ping?', 'pong!')
+    await interface.assert_message_contains('ping?')
+
+
+@test_collector()
+async def test_reaction(interface):
+    await interface.ask_human("Click check")
+    print("Done")
+
 
 # Make it easy to run the tests
 
 if __name__ == '__main__':
-	_, target_name, token = sys.argv
-	dismock.run_interactive_bot(target_name, token, testcollector)
+    _, target_name, token = sys.argv
+    dismock.run_interactive_bot(target_name, token, test_collector)
