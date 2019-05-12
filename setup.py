4de56546a112c05d121052a9f31da79f79780814
J Cover <royalcows9@gmail.com>
2019-05-12 11:29:50 -0700

    Updated the setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,17 @@
 
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='distest',
-      version='0.1.0',
+      version='0.1.0dev',
       description='Automate the testing of discord bots',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='http://github.com/JacobCover/distest',
-      author='DXsmiley',
+      author='Jake Cover',
+      author_email='python@jakecover.me',
       license='MIT',
       packages=['distest'],
       install_requires=['discord.py'],
