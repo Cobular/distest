868d1267a1e181a76974ee6117753812c71fca0e
Jake <royalcows9@gmail.com>
2019-06-05 21:24:51 -0700

    Also moved the bot testing to a new server (possibly more info soon)

--- run_tests.sh
+++ run_tests.sh
@@ -4,4 +4,4 @@ python example_target.py $TARGET_TOKEN &
 
 sleep 5s
 
-python example_tester.py $TARGET_NAME $TESTER_TOKEN -c 495301118486904832 -r all
+python example_tester.py $TARGET_NAME $TESTER_TOKEN -c 586041924129914910 -r all
