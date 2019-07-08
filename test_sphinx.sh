7b561dd66012c104e35c4215376f9cce3ed0740d
Jake <royalcows9@gmail.com>
2019-07-08 17:35:41 -0400

    Updated travis.yml to include the sphinx test, let's see how this goes

--- test_sphinx.sh
+++ test_sphinx.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 if [[ $(git diff | grep docs/source) ]]; then
     echo "Docs Changed"
+    if [[ $(cd docs && make html | grep warning) ]]; then
+        echo "There were errors"
+        exit 1
+    else
+        echo "Docs compiled without any warnings or errors!"
+        exit 0
+    fi
 else
-    echo "Docs Didn't Change"
+    echo "Docs Didn't Change, Nothing to Test"
+    exit 0
 fi
