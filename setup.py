f9415cde814dda7640ccf75c73275cf0ce4b57e1
Jake <royalcows9@gmail.com>
2020-04-09 15:51:53 -0700

    One more try

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="distest",
-    version="0.4.3",
+    version="0.4.4",
     description="Automate the testing of discord bots... With discord bots!",
     long_description=long_description,
     long_description_content_type="text/markdown",
