9105ab7d2c0ed2196da389b36ad00f5abf8590fc
Cobular <22972550+JakeCover@users.noreply.github.com>
2021-01-31 18:01:01 -0800

    Change how setup.py gets version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 
 from setuptools import setup
-from __version__ import __version__
+from distest.__about__ import __version__
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -18,7 +18,7 @@ setup(
     author_email="python@jakecover.me",
     license="MIT",
     packages=["distest", "distest.TestInterface"],
-    install_requires=["discord.py>=1.5.0"],
+    install_requires=["discord.py>=1.5.0,<1.7.0"],
     zip_safe=False,
     classifiers=[
         'License :: OSI Approved :: MIT License',
