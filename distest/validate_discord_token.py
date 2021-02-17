809b7bf2f8f4826499c5f1f6045ad5a3e0af95d6
Sean Mullarkey <45245505+sean-keisuke@users.noreply.github.com>
2021-02-17 12:22:29 -0700

    Update validate_discord_token.py
    Discord Token now can have a prefix starting with the Letter O.

--- distest/validate_discord_token.py
+++ distest/validate_discord_token.py
@@ -16,7 +16,7 @@ def token_arg(token_value: str) -> str:
     """
 
     # Regex check
-    if not re.match(r"[MN][A-Za-z\d]{23}\.[\w-]{6}\.[\w-]{27}", token_value):
+    if not re.match(r"[MNO][A-Za-z\d]{23}\.[\w-]{6}\.[\w-]{27}", token_value):
         raise argparse.ArgumentTypeError(
             "must be a correct Discord bot token.".format(token_value)
         )
