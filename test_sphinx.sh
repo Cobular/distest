aa4bfe26cd3cfa1b6c107ee559883c53131eaa21
Cobular <22972550+JakeCover@users.noreply.github.com>
2021-03-04 16:27:11 -0800

    More actions stuff :P

--- test_sphinx.sh
+++ test_sphinx.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
-if [[ $(git diff HEAD^ HEAD | grep docs/source) ]]; then
-    echo "Docs Changed"
-    if [[ $(cd docs && make testhtml | grep warning) ]]; then
-        echo "There were errors"
-        exit 1
-    else
-        echo "Docs compiled without any warnings or errors!"
-        exit 0
-    fi
-else
-    echo "Docs Didn't Change, Nothing to Test"
-    exit 0
-fi
+  if [[ $(cd docs && make testhtml | grep warning) ]]; then
+      echo "There were errors"
+      exit 1
+  else
+      echo "Docs compiled without any warnings or errors!"
+      exit 0
+  fi
